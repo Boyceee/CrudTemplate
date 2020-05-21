9d6f2413f6a90460461c8f04cc93c1944bc7334c
Boyce <787645474@qq.com>
2020-05-21 18:17:24 +0800

    Add a method to get the primary key from POJO.

--- src/main/java/com/boyce/crud/template/util/ClassUtils.java
+++ src/main/java/com/boyce/crud/template/util/ClassUtils.java
@@ -1,6 +1,7 @@
 package com.boyce.crud.template.util;
 
 import com.boyce.crud.template.annotation.Column;
+import com.boyce.crud.template.annotation.Id;
 import com.boyce.crud.template.annotation.Table;
 import org.springframework.util.StringUtils;
 
@@ -23,8 +24,8 @@ public class ClassUtils {
     public static Map<String, String> getFields(Object object) {
         Field[] fields = object.getClass().getDeclaredFields();
         int maxLength = fields.length;
-        Map<String, String> map = new HashMap<>(maxLength);
-        String sqlField = "";
+        Map<String, String> map = new HashMap<>(maxLength * 2);
+        String sqlField = null;
         for (Field field : fields) {
             if (field.isAnnotationPresent(Column.class) && !StringUtils.isEmpty(sqlField = field.getAnnotation(Column.class).value())) {
                 map.put(field.getName(), sqlField);
@@ -46,12 +47,55 @@ public class ClassUtils {
         String[] splits = objectName.split("\\.");
         objectName = splits[splits.length - 1];
         Map<String, String> tableMap = new HashMap<>(1, 1);
-        String table = "";
+        String table = null;
         if (object.getClass().isAnnotationPresent(Table.class) && !StringUtils.isEmpty(table = object.getClass().getAnnotation(Table.class).value())) {
             tableMap.put(object.getClass().getName(), table);
         } else {
             tableMap.put(object.getClass().getName(), objectName);
         }
         return tableMap;
+    }
+
+    /**
+     * If there is a field annotated by @Id,return <that field's name,sql table> as <key,value>,
+     * else if there is a field named id,return <that field's name,sql table> as <key,value>,
+     * else there will be an error named can not get a field annotated by @Id or named id from the POJO.
+     *
+     * @param object
+     * @return map of <"fieldName",field's name> and <"sqlColumn",sql column>
+     */
+    public static Map<String, String> getIdColumn(Object object) {
+        Field[] fields = object.getClass().getDeclaredFields();
+        Map<String, String> map = new HashMap<>(4);
+        String sqlField = null;
+        Field annotatedField = null;
+        Field defaultField = null;
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Id.class)) {
+                annotatedField = field;
+                break;
+            }
+            if ("id".equals(field.getName())) {
+                defaultField = field;
+            }
+        }
+        if (annotatedField != null) {
+            if (annotatedField.isAnnotationPresent(Column.class) && !StringUtils.isEmpty(sqlField = annotatedField.getAnnotation(Column.class).value())) {
+                map.put("fieldName", annotatedField.getName());
+                map.put("sqlColumn", sqlField);
+            } else {
+                map.put("fieldName", annotatedField.getName());
+                map.put("sqlColumn", annotatedField.getName());
+            }
+        } else if (defaultField != null) {
+            if (defaultField.isAnnotationPresent(Column.class) && !StringUtils.isEmpty(sqlField = defaultField.getAnnotation(Column.class).value())) {
+                map.put("fieldName", defaultField.getName());
+                map.put("sqlColumn", sqlField);
+            } else {
+                map.put("fieldName", defaultField.getName());
+                map.put("sqlColumn", defaultField.getName());
+            }
+        }
+        return map;
     }
 }
