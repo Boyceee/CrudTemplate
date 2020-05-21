f7202d388382b471b96b576fa5ca04fac5880093
Boyce <787645474@qq.com>
2020-05-21 18:25:51 +0800

    Add @Id annotation to identify the primary key.

---
+++ src/main/java/com/boyce/crud/template/annotation/Id.java
@@ -0,0 +1,13 @@
+package com.boyce.crud.template.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author Boyce
+ * @version V1.0
+ * @date 2020/5/15 16:28
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Id {
+}
