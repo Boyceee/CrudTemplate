2d1caeae1e02c04df0dd4bdade382683ef1e8420
Boyce <787645474@qq.com>
2020-05-21 18:23:44 +0800

    Add the @Id annotation to the primary key.

--- src/main/java/com/boyce/crud/template/model/TestTable.java
+++ src/main/java/com/boyce/crud/template/model/TestTable.java
@@ -1,6 +1,7 @@
 package com.boyce.crud.template.model;
 
 import com.boyce.crud.template.annotation.Column;
+import com.boyce.crud.template.annotation.Id;
 import com.boyce.crud.template.annotation.Table;
 
 import java.util.Date;
@@ -12,19 +13,29 @@ import java.util.Date;
  */
 @Table(value = "test1")
 public class TestTable {
-    private String id;
+    @Id
+    @Column(value = "id")
+    private String testId;
     private String name;
     @Column(value = "save_time")
     private Date saveTime;
     @Column(value = "is_ok")
     private int isOk;
 
-    public String getId() {
-        return id;
+    public String getTestId() {
+        return testId;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setTestId(String testId) {
+        this.testId = testId;
+    }
+
+    public int getIsOk() {
+        return isOk;
+    }
+
+    public void setIsOk(int isOk) {
+        this.isOk = isOk;
     }
 
     public String getName() {
